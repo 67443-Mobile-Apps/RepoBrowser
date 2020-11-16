6938819fc5f4ac65d984b9cb0f0256bcaa7968f7
Prof. H <profh@cmu.edu>
2020-11-16 15:23:40 -0500

    Initial Commit

---
+++ RepoBrowser/ContentView.swift
@@ -0,0 +1,20 @@
+// Created for RepoBrowser on 11/16/20 
+// Using Swift 5.0 
+// Running on macOS 11.0
+// Qapla'
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
